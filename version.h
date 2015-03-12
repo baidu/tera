24a93d94e12bbf0a101497c85150362833f33227
Yan Shiguang <shiguang31@163.com>
2015-03-13 07:57:11 +0800

    Update version.h

--- version.h
+++ version.h
@@ -1,3 +1,7 @@
+ // Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved
+ // Use of this source code is governed by a BSD-style license that can be
+ // found in the LICENSE file.
+
 #ifndef _NEARLINE_VERSION_H_
 #define _NEARLINE_VERSION_H_
 extern const char kSvnInfo[];
