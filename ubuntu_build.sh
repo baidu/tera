e138b8c093d1bf4d57d6b5c680b3fe21c4df85ca
lylei049 <lylei049@hotmail.com>
2015-07-01 13:45:42 +0800

    url change

--- ubuntu_build.sh
+++ ubuntu_build.sh
@@ -27,7 +27,7 @@ wget http://download.savannah.gnu.org/releases/libunwind/libunwind-0.99-beta.tar
 tar zxvf libunwind-0.99-beta.tar.gz
 (cd libunwind-0.99-beta && ./configure CFLAGS=-U_FORTIFY_SOURCE --disable-shared --with-pic && make -j4 && sudo make install )
 
-wget https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.2.1.tar.gz
+wget --no-check-certificate https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.2.1.tar.gz
 tar zxvf gperftools-2.2.1.tar.gz
 ( cd gperftools-2.2.1 && ./configure --disable-shared --with-pic && make -j4 && sudo make install )
 
