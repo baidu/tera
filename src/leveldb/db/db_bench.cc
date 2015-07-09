cb07ae89edde9385e9d370799d1af2fb51762fd5
xupeilin <xpeilin@gmail.com>
2015-07-09 17:30:23 +0800

    add flash_env & inmem_env in db_bench.cc

--- src/leveldb/db/db_bench.cc
+++ src/leveldb/db/db_bench.cc
@@ -849,6 +849,10 @@ class Benchmark {
         // do nothing
     } else if (strncmp(FLAGS_env, "hdfs", 5) == 0) {
        options.env = EnvDfs();
+    } else if (strncmp(FLAGS_env, "flash", 4) == 0) {
+       options.env = NewFlashEnv(EnvDfs(), NULL);
+    } else if (strncmp(FLAGS_env, "inmem", 4) == 0) {
+       options.env = NewInMemoryEnv(EnvDfs());
     } else if (strncmp(FLAGS_env, "mem", 4) == 0) {
        options.env = NewMemEnv(Env::Default());
     }
