8b86f60a246760f3c57adabd3d16860d5680c332
lylei <lylei049@gmail.com>
2015-08-14 15:56:11 +0800

    Create meta.mk

---
+++ src/io/meta.mk
@@ -0,0 +1,27 @@
+* Meta
+
+class Meta:
+  void ToMetaTableKeyValue(std::string* key, std::string* value);
+
+class TalbeMeta : public Meta
+class TabletMeta : public Meta
+
+* meta_io:
+
+struct UnpackContent {
+  TablePtr* table = NULL;
+  std::vector<TabletPtr>* tablets = NULL;
+}
+
+struct ScanContent {
+  std::vector<TalbeMeta*>* table_metas = NULL;
+  std::vector<TalbetMeta*>* tablet_metas = NULL;
+}
+
+WrtieMeta(std::map<MetaPtr, bool>);
+SuspendMetaWrite(std::map<MetaPtr, WriteClosure* done, WriteClosure* done)
+UnpackMeta(UnpackContent content);
+
+ReadTable(tablename, ScanTabletRequest* request, ScanTabletReponse* response);
+ReadTablets(tablename, tablet_key_start, tablet_key_end, ScanTabletRequest* request, ScanTabletReponse* response);
+ScanMeta(ScanContent content); // 用户需要析构conetent中内容
