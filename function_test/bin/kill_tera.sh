8bd918cbaf376f9444fb13a026448fc7123b56cf
MichelleZ <zhangdaxiaomiao@163.com>
2015-10-08 14:28:22 +0800

    issue=#347 rm unused files

--- function_test/bin/kill_tera.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./config
-
-for ((i=0; i<=${TABLETNODE_NUM}; i++)); do
-    PID=`ps x | grep tera_main | grep $((PORT+i)) | awk '{print $1}'`;
-    if [ ${PID}"x" != "x" ]; then
-        kill -9 $PID;
-    fi
-done
