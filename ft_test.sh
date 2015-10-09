3f71f5a9fa1cabb7ccc2a9ef86748930a0575557
lylei <leiliyuan@baidu.com>
2015-10-09 18:52:25 +0800

    dummy

--- ft_test.sh
+++ ft_test.sh
@@ -9,8 +9,8 @@ mkdir -p tmp/data
 cp -r test/testcase tmp/bin
 
 cd tmp/bin/
-sh kill_tera.sh
-sh launch_tera.sh
+#sh kill_tera.sh
+bash launch_tera.sh
 sleep 3
 
 export PYTHONPATH=$PYTHONPATH:../../thirdparty/include/; export PATH=$PATH:../../thirdparty/bin/ 
