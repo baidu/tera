79c6a4a83971663a7640b3b74e64810b7612ef51
bluebore <shiguang31@163.com>
2015-12-02 07:16:35 +0800

    Ajust GetDataSize log level

--- src/io/tablet_io.cc
+++ src/io/tablet_io.cc
@@ -528,7 +528,7 @@ bool TabletIO::GetDataSize(uint64_t* size, std::vector<uint64_t>* lgsize,
     }
 
     m_db->GetApproximateSizes(size, lgsize);
-    VLOG(6) << "GetDataSize(" << m_tablet_path << ") : " << *size;
+    VLOG(10) << "GetDataSize(" << m_tablet_path << ") : " << *size;
     {
         MutexLock lock(&m_mutex);
         m_db_ref_count--;
