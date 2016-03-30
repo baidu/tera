da20a66429fa8cbb0635516fba15bbdab3f7451d
taocp <taocipian@gmail.com>
2016-03-30 11:20:47 +0800

    issue=#795 bugfix.manual-compact-deadlock
    https://github.com/google/leveldb/commit/0cfb990d58ffba9b56df6e9829ddb9d220824066

--- src/leveldb/db/db_impl.cc
+++ src/leveldb/db/db_impl.cc
@@ -653,15 +653,17 @@ void DBImpl::TEST_CompactRange(int level, const Slice* begin,const Slice* end) {
   }
 
   MutexLock l(&mutex_);
-  while (!manual.done) {
-    while (manual_compaction_ != NULL) {
-      bg_cv_.Wait();
+  while (!manual.done && !shutting_down_.Acquire_Load() && bg_error_.ok()) {
+    if (manual_compaction_ == NULL) { // Idle
+        manual_compaction_ = &manual;
+        MaybeScheduleCompaction();
+    } else { // Running either my compaction or another compaction.
+        bg_cv_.Wait();
     }
-    manual_compaction_ = &manual;
-    MaybeScheduleCompaction();
-    while (manual_compaction_ == &manual) {
-      bg_cv_.Wait();
-    }
+  }
+  if (manual_compaction_ == &manual) {
+    // Cancel my manual compaction since we aborted early for some reason.
+    manual_compaction_ = NULL;
   }
 }
 
