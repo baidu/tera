2bb699b29c8243c7e64b1ecb798495691fe7d11f
lylei <leiliyuan@baidu.com>
2016-04-14 15:25:13 +0800

    gc strategy set to default

--- src/tera_flags.cc
+++ src/tera_flags.cc
@@ -103,7 +103,7 @@ DEFINE_string(tera_master_meta_table_path, "meta", "the path of meta table");
 DEFINE_int64(tera_master_split_tablet_size, 512, "the size (in MB) of tablet to trigger split");
 DEFINE_bool(tera_master_merge_enabled, false, "enable the auto-merge tablet");
 DEFINE_int64(tera_master_merge_tablet_size, 0, "the size (in MB) of tablet to trigger merge");
-DEFINE_string(tera_master_gc_strategy, "incremental", "gc strategy, [default, incremental]");
+DEFINE_string(tera_master_gc_strategy, "default", "gc strategy, [default, incremental]");
 
 DEFINE_int32(tera_master_max_split_concurrency, 1, "the max concurrency of tabletnode for split tablet");
 DEFINE_int32(tera_master_max_load_concurrency, 5, "the max concurrency of tabletnode for load tablet");
