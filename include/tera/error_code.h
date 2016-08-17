7740410857e30ef4bf7e919d7e09a6cb1a1c1abd
xupeilin <xpeilin@gmail.com>
2016-08-17 15:44:43 +0800

    modify error_code comments #964

--- include/tera/error_code.h
+++ include/tera/error_code.h
@@ -2,39 +2,41 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef  TERA_ERROR_CODE_
-#define  TERA_ERROR_CODE_
+#ifndef  TERA_ERROR_CODE_H_
+#define  TERA_ERROR_CODE_H_
 
-#include <stdint.h>
-#include <list>
-#include <map>
-#include <set>
 #include <string>
-#include <vector>
 
 #pragma GCC visibility push(default)
+
 namespace tera {
 
 class ErrorCode {
 public:
     enum ErrorCodeType {
-        kOK = 0,
-        kNotFound,
-        kBadParam,
-        kSystem,
-        kTimeout,
-        kBusy,
-        kNoQuota,
-        kNoAuth,
-        kUnknown,
-        kNotImpl,
-        kTxnFail
+        kOK        = 0,
+        kNotFound  = 1,
+        kBadParam  = 2,
+        kSystem    = 3,
+        kTimeout   = 4,
+        kBusy      = 5,
+        kNoQuota   = 6,
+        kNoAuth    = 7,
+        kUnknown   = 8,
+        kNotImpl   = 9,
+        kTxnFail   = 10
     };
-    ErrorCode();
+
+public:
+    // Return a string include type&reason
+    // Format: "type [kOK], reason [success]"
     std::string ToString() const;
 
-    std::string GetReason() const;
     ErrorCodeType GetType() const;
+    std::string GetReason() const;
+
+    // Internal funcion, do not use
+    ErrorCode();
     void SetFailed(ErrorCodeType err, const std::string& reason = "");
 
 private:
@@ -42,9 +44,9 @@ private:
     std::string _reason;
 };
 
+// deprecated, try error_code.ToString()
 const char* strerr(ErrorCode error_code);
 
 } // namespace tera
 #pragma GCC visibility pop
-
-#endif  // TERA_ERROR_CODE_
+#endif  // TERA_ERROR_CODE_H_
