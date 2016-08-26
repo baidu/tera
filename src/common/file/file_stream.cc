a3ca1c7afe848abea2dd4dc5e31124907e5a54f6
xupeilin <xpeilin@gmail.com>
2016-08-26 16:59:42 +0800

    member variable style opz - common #986

--- src/common/file/file_stream.cc
+++ src/common/file/file_stream.cc
@@ -17,13 +17,13 @@
 DECLARE_int32(file_op_retry_times);
 
 FileStream::FileStream()
-    : m_fp(NULL) {}
+    : fp_(NULL) {}
 
 bool FileStream::Open(const std::string& file_path, FileOpenMode flag,
                             FileErrorCode* error_code) {
     std::string open_mode = FileOpenModeToString(flag);
-    m_fp = fopen(file_path.c_str(), open_mode.c_str());
-    if (m_fp == NULL) {
+    fp_ = fopen(file_path.c_str(), open_mode.c_str());
+    if (fp_ == NULL) {
         SetErrorCode(error_code, kFileErrOpenFail);
         return false;
     }
@@ -33,14 +33,14 @@ bool FileStream::Open(const std::string& file_path, FileOpenMode flag,
 }
 
 bool FileStream::Close(FileErrorCode* error_code) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         SetErrorCode(error_code, kFileErrNotOpen);
         return false;
     }
 
     Flush();
 
-    if (fclose(m_fp) != 0) {
+    if (fclose(fp_) != 0) {
         LOG(ERROR) << "fail to close file, errno: " << strerror(errno);
         SetErrorCode(error_code, kFileErrClose);
         return false;
@@ -50,7 +50,7 @@ bool FileStream::Close(FileErrorCode* error_code) {
 
 int64_t FileStream::Read(void* buffer, int64_t buffer_size,
                          FileErrorCode* error_code) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         SetErrorCode(error_code, kFileErrNotOpen);
         return -1;
     }
@@ -59,10 +59,10 @@ int64_t FileStream::Read(void* buffer, int64_t buffer_size,
         return -1;
     }
 
-    int64_t read_bytes = fread(buffer, 1, static_cast<size_t>(buffer_size), m_fp);
+    int64_t read_bytes = fread(buffer, 1, static_cast<size_t>(buffer_size), fp_);
     bool success = true;
     if (read_bytes != buffer_size) {
-        if (ferror(m_fp)) success = false;
+        if (ferror(fp_)) success = false;
     }
     if (success) {
         SetErrorCode(error_code, kFileSuccess);
@@ -75,7 +75,7 @@ int64_t FileStream::Read(void* buffer, int64_t buffer_size,
 
 int64_t FileStream::Write(const void* buffer, int64_t buffer_size,
                           FileErrorCode* error_code) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         SetErrorCode(error_code, kFileErrNotOpen);
         return -1;
     }
@@ -90,7 +90,7 @@ int64_t FileStream::Write(const void* buffer, int64_t buffer_size,
         size_t expect_size = static_cast<size_t>(buffer_size - total_size);
         if (expect_size == 0u) break;
 
-        size_t ret_size = fwrite(byte_buf + total_size, 1, expect_size, m_fp);
+        size_t ret_size = fwrite(byte_buf + total_size, 1, expect_size, fp_);
         total_size += static_cast<int64_t>(ret_size);
 
         if (ret_size < expect_size) {
@@ -100,7 +100,7 @@ int64_t FileStream::Write(const void* buffer, int64_t buffer_size,
                 << ", expect_size = " << expect_size
                 << ", this writen size = " << ret_size
                 << "], reason: " << strerror(errno);
-            CHECK(ferror(m_fp)) << "file writer is broken";
+            CHECK(ferror(fp_)) << "file writer is broken";
             if (errno != EINTR && ret_size == 0u) break;
         }
     }
@@ -114,17 +114,17 @@ int64_t FileStream::Write(const void* buffer, int64_t buffer_size,
 }
 
 bool FileStream::Flush() {
-    if (m_fp ==  NULL) {
+    if (fp_ ==  NULL) {
         return false;
     }
-    return  (fflush(m_fp) == 0) && (fsync(fileno(m_fp)) == 0);
+    return  (fflush(fp_) == 0) && (fsync(fileno(fp_)) == 0);
 }
 
 int64_t FileStream::Seek(int64_t offset, int32_t origin, FileErrorCode* error_code) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         return -1;
     }
-    if (fseeko(m_fp, offset, origin) < 0) {
+    if (fseeko(fp_, offset, origin) < 0) {
 //         SetErrorCode(error_code, errono);
         return -1;
     }
@@ -132,10 +132,10 @@ int64_t FileStream::Seek(int64_t offset, int32_t origin, FileErrorCode* error_co
 }
 
 int64_t FileStream::Tell(FileErrorCode* error_code) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         return -1;
     }
-    int64_t ret = ftello(m_fp);
+    int64_t ret = ftello(fp_);
     if (ret < 0) {
         // set error code
     } else {
@@ -178,24 +178,24 @@ int64_t FileStream::GetSize(const std::string& file_path,
 }
 
 int32_t FileStream::ReadLine(void* buffer, int32_t max_size) {
-    if (m_fp == NULL) {
+    if (fp_ == NULL) {
         return -1;
     }
 
     if (!buffer || max_size <= 0) return -1;
     char* read_buffer = static_cast<char*>(buffer);
-    off64_t org_offest = ftello(m_fp);
+    off64_t org_offest = ftello(fp_);
     if (org_offest < 0)
         return -1;
-    char* readed_buffer = fgets(read_buffer, max_size, m_fp);
+    char* readed_buffer = fgets(read_buffer, max_size, fp_);
     if (readed_buffer == NULL) {
-        if (feof(m_fp)) {
+        if (feof(fp_)) {
             return 0;
         } else {
             return -1;
         }
     } else {
-        off64_t new_offset = ftello(m_fp);
+        off64_t new_offset = ftello(fp_);
         if (new_offset < 0)
             return -1;
         return new_offset - org_offest;
