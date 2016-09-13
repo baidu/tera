cd87cf240e7876efbfc01af5064184042660a703
taocp <taocipian@gmail.com>
2016-09-13 19:59:03 +0800

    issue=#841 log remote address

---
+++ src/utils/network_utils.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2016, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "utils/network_utils.h"
+
+namespace tera {
+namespace utils {
+
+std::string GetRemoteAddress(google::protobuf::RpcController* controller) {
+    assert(controller != NULL);
+    sofa::pbrpc::RpcController* cntl = static_cast<sofa::pbrpc::RpcController*>(controller);
+    return cntl->RemoteAddress();
+}
+
+} // namespace utils
+} // namepsace tera
