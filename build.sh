7d6a154049487dc037060204c562e7b94f536704
taocp <taocipian@gmail.com>
2016-10-19 00:18:20 -0800

    issue=#18 update build.sh and support Gentoo/Linux

--- build.sh
+++ build.sh
@@ -22,13 +22,13 @@ fi
 cd ${DEPS_SOURCE}
 
 # boost
-if [ ! -f "${FLAG_DIR}/boost_1_57_0" ] \
-    || [ ! -d "${DEPS_PREFIX}/boost_1_57_0/boost" ]; then
-    wget -O boost_1_57_0.tar.gz http://superb-dca2.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
-    tar zxf boost_1_57_0.tar.gz
-    rm -rf ${DEPS_PREFIX}/boost_1_57_0
-    mv boost_1_57_0 ${DEPS_PREFIX}
-    touch "${FLAG_DIR}/boost_1_57_0"
+if [ ! -f "${FLAG_DIR}/boost_1_58_0" ] \
+    || [ ! -d "${DEPS_PREFIX}/boost_1_58_0/boost" ]; then
+    wget -O boost_1_58_0.tar.bz2 http://mirrors.163.com/gentoo/distfiles/boost_1_58_0.tar.bz2
+    tar xjf boost_1_58_0.tar.bz2
+    rm -rf ${DEPS_PREFIX}/boost_1_58_0
+    mv boost_1_58_0 ${DEPS_PREFIX}
+    touch "${FLAG_DIR}/boost_1_58_0"
 fi
 
 # protobuf
@@ -75,13 +75,10 @@ if [ ! -f "${FLAG_DIR}/sofa-pbrpc_1_1_0" ] \
     sed -i '/BOOST_HEADER_DIR=/ d' depends.mk
     sed -i '/PROTOBUF_DIR=/ d' depends.mk
     sed -i '/SNAPPY_DIR=/ d' depends.mk
-    echo "BOOST_HEADER_DIR=${DEPS_PREFIX}/boost_1_57_0" >> depends.mk
+    echo "BOOST_HEADER_DIR=${DEPS_PREFIX}/boost_1_58_0" >> depends.mk
     echo "PROTOBUF_DIR=${DEPS_PREFIX}" >> depends.mk
     echo "SNAPPY_DIR=${DEPS_PREFIX}" >> depends.mk
     echo "PREFIX=${DEPS_PREFIX}" >> depends.mk
-    cd src
-    PROTOBUF_DIR=${DEPS_PREFIX} sh compile_proto.sh
-    cd ..
     make -j4
     make install
     cd ..
@@ -89,21 +86,22 @@ if [ ! -f "${FLAG_DIR}/sofa-pbrpc_1_1_0" ] \
 fi
 
 # zookeeper
-if [ ! -f "${FLAG_DIR}/zookeeper_3_4_6" ] \
+if [ ! -f "${FLAG_DIR}/zookeeper_3_4_9" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libzookeeper_mt.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/zookeeper" ]; then
-    wget -O zookeeper-3.4.6.tar.gz http://www.us.apache.org/dist/zookeeper/stable/zookeeper-3.4.6.tar.gz
-    tar zxf zookeeper-3.4.6.tar.gz
-    cd zookeeper-3.4.6/src/c
+    wget -O zookeeper-3.4.9.tar.gz http://mirrors.163.com/gentoo/distfiles/zookeeper-3.4.9.tar.gz
+    tar zxf zookeeper-3.4.9.tar.gz
+    cd zookeeper-3.4.9/src/c
     ./configure ${DEPS_CONFIG}
     make -j4
     make install
     cd -
-    touch "${FLAG_DIR}/zookeeper_3_4_6"
+    touch "${FLAG_DIR}/zookeeper_3_4_9"
 fi
 
 # cmake for gflags
-if ! which cmake ; then
+if [ ! -f "${FLAG_DIR}/cmake_3_2_1" ] \
+    || [ ! -f "${DEPS_PREFIX}/bin/cmake" ]; then
     wget --no-check-certificate -O CMake-3.2.1.tar.gz https://github.com/Kitware/CMake/archive/v3.2.1.tar.gz
     tar zxf CMake-3.2.1.tar.gz
     cd CMake-3.2.1
@@ -111,6 +109,7 @@ if ! which cmake ; then
     make -j4
     make install
     cd -
+    touch "${FLAG_DIR}/cmake_3_2_1"
 fi
 
 # gflags
@@ -145,17 +144,23 @@ fi
 if [ ! -f "${FLAG_DIR}/gtest_1_7_0" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libgtest.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/gtest" ]; then
-    # wget --no-check-certificate https://googletest.googlecode.com/files/gtest-1.7.0.zip
-    rm -rf gtest_archive gtest-1.7.0.zip gtest-1.7.0
-    git clone --depth=1 https://github.com/xupeilin/gtest_archive
-    mv gtest_archive/gtest-1.7.0.zip .
-    unzip gtest-1.7.0.zip
-    cd gtest-1.7.0
-    ./configure ${DEPS_CONFIG}
+    rm -rf ./googletest-release-1.7.0
+    wget --no-check-certificate -O googletest-release-1.7.0.zip https://codeload.github.com/google/googletest/zip/release-1.7.0
+    unzip googletest-release-1.7.0.zip
+    cd googletest-release-1.7.0
+
+    # XXX make gcc3 happy; what's the cmake way?
+    sed -i 's/ -Wno-missing-field-initializers/ /' cmake/internal_utils.cmake
+
+    mkdir tmpbuild
+    cd tmpbuild
+    cmake -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" ..
     make
-    cp -af lib/.libs/* ${DEPS_PREFIX}/lib
+    cd ..
+    cp -af tmpbuild/libgtest.a ${DEPS_PREFIX}/lib
+    cp -af tmpbuild/libgtest_main.a ${DEPS_PREFIX}/lib
     cp -af include/gtest ${DEPS_PREFIX}/include
-    cd -
+    cd ..
     touch "${FLAG_DIR}/gtest_1_7_0"
 fi
 
@@ -163,9 +168,9 @@ fi
 if [ ! -f "${FLAG_DIR}/libunwind_0_99_beta" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libunwind.a" ] \
     || [ ! -f "${DEPS_PREFIX}/include/libunwind.h" ]; then
-    wget -O libunwind-0.99-beta.tar.gz http://download.savannah.gnu.org/releases/libunwind/libunwind-0.99-beta.tar.gz
-    tar zxf libunwind-0.99-beta.tar.gz
-    cd libunwind-0.99-beta
+    wget http://mirrors.163.com/gentoo/distfiles/libunwind-0.99.tar.gz
+    tar xzf libunwind-0.99.tar.gz
+    cd libunwind-0.99
     ./configure ${DEPS_CONFIG}
     make CFLAGS=-fPIC -j4
     make CFLAGS=-fPIC install
@@ -198,7 +203,7 @@ if [ ! -f "${FLAG_DIR}/ins_0_13" ] \
     cd ins-0.13
     sed -i "s|^PREFIX=.*|PREFIX=${DEPS_PREFIX}|" Makefile
     sed -i "s|^PROTOC=.*|PROTOC=${DEPS_PREFIX}/bin/protoc|" Makefile
-    BOOST_PATH=${DEPS_PREFIX}/boost_1_57_0 make install_sdk
+    BOOST_PATH=${DEPS_PREFIX}/boost_1_58_0 make install_sdk
     make -j4 install_sdk
     cd -
     touch "${FLAG_DIR}/ins_0_13"
@@ -218,7 +223,7 @@ sed -i "s:^GFLAGS_PREFIX=.*:GFLAGS_PREFIX=$DEPS_PREFIX:" depends.mk
 sed -i "s:^GLOG_PREFIX=.*:GLOG_PREFIX=$DEPS_PREFIX:" depends.mk
 sed -i "s:^GTEST_PREFIX=.*:GTEST_PREFIX=$DEPS_PREFIX:" depends.mk
 sed -i "s:^GPERFTOOLS_PREFIX=.*:GPERFTOOLS_PREFIX=$DEPS_PREFIX:" depends.mk
-sed -i "s:^BOOST_INCDIR=.*:BOOST_INCDIR=$DEPS_PREFIX/boost_1_57_0:" depends.mk
+sed -i "s:^BOOST_INCDIR=.*:BOOST_INCDIR=$DEPS_PREFIX/boost_1_58_0:" depends.mk
 sed -i "s:^INS_PREFIX=.*:INS_PREFIX=$DEPS_PREFIX:" depends.mk
 
 ########################################
